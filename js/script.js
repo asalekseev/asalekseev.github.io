bbeb2ab938f2573376a18c4021e0a23646d5e4f5
asalekseev <90420814+asalekseev@users.noreply.github.com>
2021-09-15 00:16:13 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,14 +1,44 @@
 window.addEventListener('DOMContentLoaded', () => {
     items = document.querySelectorAll('.systems_item_card');
+    radio1 = document.querySelectorAll('.form_group2_radiochecks_check');
+    radio2 = document.querySelectorAll('.form_group2_radiochecks_checked');
 
     items.forEach(item => {
         item.addEventListener('mouseover', () => {
-            item.classList.toggle('active');
             item.className = 'systems_item_card_active';
         });
         item.addEventListener('mouseout', () => {
-            item.classList.toggle('active');
             item.className = 'systems_item_card';
+        });
+    })
+
+    radio1.forEach(item => {
+        item.addEventListener('click', () => {
+            if (item.className == 'form_group2_radiochecks_checked')
+            {
+                item.className = 'form_group2_radiochecks_check';
+                document.getElementsByName('radio'+item.id).value=0;
+            }
+            else
+            {
+                item.className = 'form_group2_radiochecks_checked';
+                document.getElementsByName('radio'+item.id).value=1;
+            }
+        });
+    })
+
+    radio2.forEach(item => {
+        item.addEventListener('click', () => {
+            if (item.className == 'form_group2_radiochecks_checked')
+            {
+                item.className = 'form_group2_radiochecks_check';
+                document.getElementsByName('radio'+item.id).value=0;
+            }
+            else
+            {
+                item.className = 'form_group2_radiochecks_checked';
+                document.getElementsByName('radio'+item.id).value=1;
+            }
         });
     })
 })
